6071114e1e27adc0bae1e2a198dbb201fddfd683
lordfuoco <lordfuoco@gmail.com>
2018-01-12 15:52:52 +0100

    Added dist folder to the repo, proper config for cjs

--- poi.config.js
+++ poi.config.js
@@ -1,9 +1,12 @@
 module.exports = (options, req) => ({
   entry: './src/index.js',
   vendor: false,
+  sourceMap: false,
+  html: false,
+  format: 'cjs',
   filename: {
     js: 'django-feedback.js',
-    css: 'style.css',
+    css: 'django-feedback.css',
     images: 'assets/images/[name].[ext]',
     fonts: 'assets/fonts/[name].[ext]',
     chunk: '[id].chunk.js'
